e44791945281be8401c1f006193e9a0fceb75bb4
laithong SINGHALAT <laithong@STB.LA>
2021-09-20 17:04:12 +0700

    DB SETUP

---
+++ back-end/database/migrations/1632127514880_hrtb_contactworks.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class HrtbContactworks extends BaseSchema {
+  protected tableName = 'hrtb_contactworks'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
